661a8d94ff0b759940e011f7564cebc54ced8673
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-04-15 03:29:35 +0530

    Create IntDataType.java

---
+++ BASICS/IntDataType.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class IntDataType {
+	public static void main(String[] args){
+		int a = 45;
+		System.out.println(a); // 45
+		
+		int b = 045;
+		System.out.println(b); // 37
+		
+		int c = 0x45;
+		System.out.println(c); // 69
+		
+		int d = 0b101101;
+		System.out.println(d);  // 45 
+	}
+}
