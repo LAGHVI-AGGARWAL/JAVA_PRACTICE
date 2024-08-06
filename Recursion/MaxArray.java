3caa57a34f4dbb5652825ab95f21d92c49d75188
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-08-06 19:42:55 +0530

    Create MaxArray.java

---
+++ Recursion/MaxArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main
+{
+    public static void maxArray(int[] arr, int n, int i, int max){
+        if(i == n){
+            System.out.println(max);
+            return;
+        }
+        max = Math.max(arr[i],max);
+        maxArray(arr,n,i+1,max);
+    }
+    
+    public static int maxArray1(int arr[], int n, int i, int max){
+        if(i == n-1){
+            return arr[i];
+        }
+        maxArray(arr,n,i+1,max);
+        max = Math.max(arr[i],max);
+        return max;
+    }
+    
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int[] arr = new int[n];
+	    for(int i = 0; i < n; i++){
+	        arr[i] = sc.nextInt();
+	    }
+	    int max = Integer.MIN_VALUE;
+	    maxArray(arr,n,0,max);
+	    maxArray1(arr,n,0,max);
+	}
+}
