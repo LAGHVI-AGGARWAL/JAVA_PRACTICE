d505c6e69c3de12c5bcc4785f89e49719ec1b0ac
LAGHVI-AGGARWAL <145498319+LAGHVI-AGGARWAL@users.noreply.github.com>
2024-08-21 19:11:52 +0530

    Create Transpose.java

---
+++ Arrays/2DArrays/Transpose.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int[][] arr = {{1,2},{3,4},{5,6}};
+	    int m = arr.length;
+	    int n = arr[0].length;
+	    int[][] transpose = new int[n][m];
+	   // for(int i = 0; i < m; i++){
+	   //     for(int j = 0; j < n; j++){
+	   //        System.out.print(arr[i][j] + " ");
+	   //     }
+	   // }
+	    
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < m; j++){
+	            transpose[i][j] = arr[j][i]; 
+	        }
+	    }
+	    
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < m; j++){
+	           System.out.print(transpose[i][j] + " ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
